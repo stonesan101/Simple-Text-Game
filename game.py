ccd7e4733ec0b9a432eb9b2b1242a09c9ca65a6f
stonesan101 <77947977+stonesan101@users.noreply.github.com>
2025-09-23 08:10:18 -0500

    Add files via upload
    Fix error with final function

--- game.py
+++ game.py
@@ -1,28 +1,30 @@
 def retry():
     choice = input("Try Again? y/n: ")
+    # restart game
     if choice.lower() == "y":
-        intro(1, 1)
+        partOne(1, 1)
+    # end game
     else:
         print("\nThank you for playing!")
 
 
-def easyEnding(ending, level, strength):
-    print(f"You choose the easy way out.\n{ending}\nEnding Level: {level}\nEnding Strength: {strength}\nGame Over")
-    retry()
-
-
 # 3c
-def whileLoop(inputText, endingText, continueInput, endingInput, nextFun, level, strength):
+def inputLoop(inputText, endingText, continueInput, endingInput, nextFun, level, strength):
     # 2b
     while (True):
         choice = input(inputText).lower()
+        # run next function
         if choice == continueInput:
             nextFun(level, strength)
             # 2c
             break
+        # early ending
         if choice == endingInput:
-            easyEnding(endingText, level, strength)
+            print(
+                f"You choose the easy way out.\n{endingText}\nEnding Level: {level}\nEnding Strength: {strength}\nGame Over")
+            retry()
             break
+        # invalid input
         else:
             print(f"\nInvalid Input\nPlease choose {continueInput} or {endingInput}\n")
             # 2d
@@ -30,71 +32,88 @@ def whileLoop(inputText, endingText, continueInput, endingInput, nextFun, level,
 
 
 # 3f
-def intro(level=1, strength=1):
+def partOne(level=1, strength=1):
+    printStars()
+
     print(
         f"You stand in a field.\n To the west you see a path sunny and bright.\n To the east an omnibus dark tunnel.\n")
-    choice = input('Do you go east or west:')
-    if choice == 'west':
-        easyEnding("You go for a stroll in the woods and return home.", level, strength)
-    elif choice == 'east':
-        secondChoice(level, strength)
-    else:
-        print("\nInvalid entry")
-        # 4b
-        intro()
+
+    inputText = 'Do you go east or west:'
+    endingText = "You go for a stroll in the woods and return home."
+    continueInput = 'east'
+    endingInput = 'west'
+    inputLoop(inputText, endingText, continueInput, endingInput, partTwo, level, strength)
 
 
-def secondChoice(level, strength):
+def partTwo(level, strength):
     print("\nYou walk down the dark tunnel for quite a while before encountering a slime monster\nYou are unarmed\n")
+
     inputText = 'Do you run or fight: '
     endingText = 'You run like a sissy back out of the tunnel'
     continueInput = 'fight'
     endingInput = 'run'
-    whileLoop(inputText, endingText, continueInput, endingInput, thirdChoice, level, strength)
+    inputLoop(inputText, endingText, continueInput, endingInput, partThree, level, strength)
 
 
-def thirdChoice(level, strength):
+def partThree(level, strength):
     print(
         '\nYou heroically play Fisticuffs with the slime\nAfter a long fight the slime you finally defeat the slime!!\nLevel +1\n\nYou notice a chest behind the dead slime and open it.\nInside you find a Bronze Sword\nStrength +5\n')
+
     level += 1
     strength += 5
+
     inputText = 'Do you continue or leave: '
     endingText = 'You exit the tunnel feeling only slightly like a sissy.'
     continueInput = 'continue'
     endingInput = 'leave'
-    whileLoop(inputText, endingText, continueInput, endingInput, fourthChoice, level, strength)
+    inputLoop(inputText, endingText, continueInput, endingInput, partFour, level, strength)
 
 
-def fourthChoice(level, strength):
+def partFour(level, strength):
     print(
         "\nYou reach the end of the tunnel and find another chest.\nJust as you touch the chest you're startled by a loud thud behind you!\nYou Turn around to find the skeleton knight 'Mr Boney Pants Guy'\nWith your exit blocked you have no choice but to fight!\nIt's a downhill battle, you're exhausted and about to faint\nYou manage to get behind the enemy, finally you have a way out\n")
-    inputText = 'Do you flee or fight?'
+
+    inputText = 'Do you flee or fight: '
     endingText = "You barely manage to escape, you lie on the grass out of breath"
     continueInput = 'fight'
     endingInput = 'flee'
-    whileLoop(inputText, endingText, continueInput, endingInput, finalEnding, level, strength)
+    inputLoop(inputText, endingText, continueInput, endingInput, partFive, level, strength)
 
 
-# 3a
-def finalEnding():
+def partFive(level, strength):
+    print(
+        f"\nCRITICAL HIT!\nMr Boney Pants Guy's bones crumble to the ground\nLevel +2\nStrength +10\n\nEnding Level: {level+2}\nEnding Strength: {strength+10}\nThanks for playing!")
+
     choice = input("Try Again? y/n: ")
     if choice.lower() == "y":
-        intro(1, 1)
+        partOne()
     else:
         print("\nThank you for playing!")
 
 
-for i in range(5):
-    # 2e
-    for j in range(25):
-        print("*", end=" ")
-    print()
+# 3a
+def printStars():
+    # 2a
+    for i in range(2):
+        # 2e
+        for j in range(40):
+            print("*", end=" ")
+        print()
 
-intro()
 
-# 2a
-for i in range(5):
-    # 2e
-    for j in range(25):
-        print("*", end=" ")
-    print()
+partOne()
+
+# recursive version of inputLoop
+def recursiveLoop(inputText, endingText, continueInput, endingInput, nextFun, level, strength):
+    choice = input(inputText).lower()
+    if choice == continueInput:
+        nextFun(level, strength)
+    if choice == endingInput:
+        print(
+            f"You choose the easy way out.\n{endingText}\nEnding Level: {level}\nEnding Strength: {strength}\nGame Over")
+        retry()
+
+    else:
+        print(f"\nInvalid Input\nPlease choose {continueInput} or {endingInput}\n")
+        # 4b
+        recursiveLoop(inputText, endingText, continueInput, endingInput, nextFun, level, strength)
